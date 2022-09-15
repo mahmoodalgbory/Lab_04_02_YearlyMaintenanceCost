6f15dff2ed377891689d3bf6a08c776390e82565
mahmoodalgbory <algbormd@mail.uc.edu>
2022-09-15 18:56:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args)
+    {
+    double summerCost = 1143.29;
+    double fallCost = 2492.13;
+    double winterCost = 3257.72;
+    double springCost = 4201.94;
+    double totalCost = summerCost + fallCost + winterCost + springCost;
+
+    System.out.println("The summer maintenance cost is " + summerCost);
+        System.out.println("The fall maintenance cost is " + fallCost);
+        System.out.println("The winter maintenance cost is " + winterCost);
+        System.out.println("The spring maintenance cost is " + springCost);
+        System.out.println("The total maintenance cost is " + totalCost);
+    }
+}
